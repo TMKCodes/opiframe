d1cf06167618b6d141c92e32f16fd61282e8afdb
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2022-08-29 13:20:01 +0300

    add dynamic route to express helloworld

--- Javascript/01-helloworld/server.js
+++ Javascript/01-helloworld/server.js
@@ -4,6 +4,10 @@ let app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, function() {
+app.get("/greeting/:name", (req, res) => {
+  return res.status(200).json({ "greeting" : "Hello " + req.params.name });
+});
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
